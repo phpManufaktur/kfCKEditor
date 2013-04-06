46ad26f64d4e3f5a56859cd2c121073ce547bb5d
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-04-06 04:48:50 +0200

    fixed a typo

--- CKEditor/bootstrap.include.php
+++ CKEditor/bootstrap.include.php
@@ -4,7 +4,7 @@
  * CKEditor extension for the kitFramework
  *
  * @author Team phpManufaktur <team@phpmanufaktur.de>
- * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @link https://addons.phpmanufaktur.de/kfCKEditor
  * @copyright 2013 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
