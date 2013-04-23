d5711a3b7a4bd0ef9c94148f5df00c0a63cb47f1
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-04-23 05:34:37 +0200

    extended Twig implementation with paramters for width, height and custom configuration files

--- bootstrap.include.php
+++ bootstrap.include.php
@@ -13,15 +13,19 @@
 // add the function CKEditor to Twig
 $app['twig'] = $app->share($app->extend('twig', function  ($twig, $app) {
 
-    $twig->addFunction(new Twig_SimpleFunction('CKEditor', function ($id, $name, $content) {
+    $twig->addFunction(new Twig_SimpleFunction('CKEditor', function ($id, $name, $content, $width='100%', $height='200px', $config='default') {
 
         $framework_url = FRAMEWORK_URL;
         $image_url = FRAMEWORK_URL.'/admin/mediabrowser/cke';
+        $config_url = ($config == 'default') ? MANUFAKTUR_URL.'/CKEditor/ckeditor_config.js' : $config;
 
         return <<<EOD
-        <textarea id="$id" name="$name">$content</textarea>
+        <textarea class="ckeditor" id="$id" name="$name">$content</textarea>
         <script>
           CKEDITOR.replace('$id', {
+            width: '$width',
+            height: '$height',
+            customConfig: '$config_url',
             baseHref: '$framework_url',
             filebrowserImageBrowseUrl: '$image_url',
             filebrowserWindowWidth: 900,
